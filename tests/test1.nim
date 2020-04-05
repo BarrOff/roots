770f88cf5ef75f89f9dfc43cd21b73e1ec5f18d6
BarrOff <58253563+BarrOff@users.noreply.github.com>
2020-04-05 17:01:42 +0200

    move options variable into setup

--- tests/test1.nim
+++ tests/test1.nim
@@ -110,34 +110,28 @@ suite "kwargs Tests":
   setup:
     proc f(x: float): float =
       return exp(x) - x^4
-
-  test "kwargs without algorithm":
     let
       kw = UnivariateZeroOptions[float, float, float, float]()
+
+  test "kwargs without algorithm":
     initOptions2(kw, Bisection())
     let
       z = findZero(f, (8.0, 9.0), kwargs = kw)
     check(z == 8.613169456441399)
 
   test "kwargs with A42":
-    let
-      kw = UnivariateZeroOptions[float, float, float, float]()
     initOptions2(kw, A42())
     let
       z = findZero(f, (8.0, 9.0), A42(), kwargs = kw)
     check(z == 8.613169456441399)
 
   test "kwargs with AlefeldPotraShi":
-    let
-      kw = UnivariateZeroOptions[float, float, float, float]()
     initOptions2(kw, AlefeldPotraShi())
     let
       z = findZero(f, (8.0, 9.0), AlefeldPotraShi(), kwargs = kw)
     check(z == 8.613169456441399)
 
   test "kwargs with Bisection":
-    let
-      kw = UnivariateZeroOptions[float, float, float, float]()
     initOptions2(kw, Bisection())
     let
       z1 = findZero(f, (8.0, 9.0), kwargs = kw)
@@ -146,8 +140,6 @@ suite "kwargs Tests":
     check(z2 == 8.613169456441399)
 
   test "kwargs with BisectionExact":
-    let
-      kw = UnivariateZeroOptions[float, float, float, float]()
     initOptions2(kw, BisectionExact())
     let
       z = findZero(f, (8.0, 9.0), BisectionExact(), kwargs = kw)
