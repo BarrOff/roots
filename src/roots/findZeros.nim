29131443199874f7c7cd7c2e7b76ce6b445a25a7
BarrOff <58253563+BarrOff@users.noreply.github.com>
2020-09-03 18:32:17 +0200

    add documentation for findZeros.nim

--- src/roots/findZeros.nim
+++ src/roots/findZeros.nim
@@ -1,29 +1,76 @@
+## =========
+## findZeros
+## =========
+##
+##Attempt to find all zeros in an interval (a,b)
+##
+## Algorithm
+## =========
+##
+## The basic algorithm checks for zeros among the endpoints, and then
+## divides the interval `(a,b)` into `no_pts-1` subintervals and then
+## proceeds to look for zeros through bisection or a derivative-free
+## method.  As checking for a bracketing interval is relatively cheap and
+## bisection is guaranteed to converge, each interval has `k` pairs of
+## intermediate points checked for a bracket.
+##
+## If any zeros are found, the algorithm uses these to partition `(a,b)`
+## into subintervals. Each subinterval is shrunk so that the endpoints
+## are not zeros and the process is repeated on the subinterval. If the
+## initial interval is too large, then the naive scanning for zeros may
+## be fruitless and no zeros will be reported. If there are nearby zeros,
+## the shrinking of the interval may jump over them, though as seen in
+## the examples, nearby roots can be identified correctly, though for
+## really nearby points, or very flat functions, it may help to increase
+## `no_pts`.
+##
+## The tolerances are used to shrink the intervals, but not to find zeros
+## within a search. For searches, bisection is guaranteed to converge
+## with no specified tolerance. For the derivative free search, a
+## modification of the `Order0` method is used, which at worst case
+## compares `|f(x)| <= 8*eps(x)` to identify a zero. The algorithm might
+## identify more than one value for a zero, due to floating point
+## approximations. If a potential pair of zeros satisfy
+## `isapprox(a,b,atol=sqrt(xatol), rtol=sqrt(xrtol))` then they are
+## consolidated.
+##
+## The algorithm can make many function calls. When zeros are found in an
+## interval, the naive search is carried out on each subinterval. To cut
+## down on function calls, though with some increased chance of missing
+## some zeros, the adaptive nature can be skipped with the argument
+## `naive=true` or the number of points stepped down.
+##
+## The algorithm is derived from one in a
+## `PR [https://github.com/JuliaMath/Roots.jl/pull/113]`_ by @djsegal.
+##
+## Note
+## ====
+##
+## There are two cases where the number of zeros may be underreported:
+## * if the initial interval, [a,b], is too wide
+## * if there are zeros  that are very nearby
+
 import algorithm, math, sequtils, sugar
 import private/utils, simple
 
-# the algorithm first scans for zeros using the naive approach, then
-# splits (a,b) by these zeros. This struct holds the subintervals
 type
   Interval[T: SomeFloat] = object
+    ## the algorithm first scans for zeros using the naive approach, then
+    ## splits (a,b) by these zeros. This struct holds the subintervals
     a: T
     b: T
     depth: int
-
-# Attempt to find all zeros in an interval (a,b)
-
-# Algorithm due to @djsegal in https://github.com/JuliaMath/Roots.jl/pull/113
-
-
-# A naive approach to find zeros: split (a,b) by n points, look into each for a
-# zero * k is oversampling rate for bisection. (It is relatively cheap to check
-# for a bracket so we oversample our intervals looking for brackets
-# * assumes f(a) *not* a zero
 
 proc fz2[T, S: SomeFloat](zs: var seq[S], f: proc(x: T): S, a, b: T,
     no_pts: int, k: int = 4)
 
 proc fz[T, S: SomeFloat](f: proc(x: T): S, a, b: T, no_pts: int,
     k: int = 4): seq[S] =
+  ## A naive approach to find zeros: split (a,b) by n points, look into each for a
+  ## zero
+  ## * k is oversampling rate for bisection. (It is relatively cheap to check
+  ##   for a bracket so we oversample our intervals looking for brackets
+  ## * assumes f(a) *not* a zero
   var
     zs: seq[S]
   fz2(zs, f, a, b, no_pts, k)
@@ -87,17 +134,17 @@ proc fz2[T, S: SomeFloat](zs: var seq[S], f: proc(x: T): S, a, b: T,
 
     sort(zs)
 
-# check if f(a) is non zero using tolerances max(atol, eps()), rtol
 proc nonZero[T, S: SomeFloat](fa: S, a: T, atol, rtol: S): bool =
+  ## check if f(a) is non zero using tolerances max(atol, eps()), rtol
   when sizeof(T) == 8:
     abs(fa) >= max(atol, max(abs(a) * rtol, (nextafter(1.0, 2.0) - 1.0)))
   else:
     abs(fa) >= max(atol, max(abs(a) * rtol, (nextafterf(1.0, 2.0) - 1.0)))
 
-# After splitting by zeros we have intervals (zm, zn) this is used to shrink
-# to (zm+, zn-) where both are non-zeros, as defined above
 proc findNonZero[T, S: SomeFloat](f: proc(x: T): S, a, barrier, xatol, xrtol: T,
                                       atol, rtol: S): T =
+  ## After splitting by zeros we have intervals (zm, zn) this is used to shrink
+  ## to (zm+, zn-) where both are non-zeros, as defined above
   when sizeof(T) == 8:
     let
       xtol = max(xatol, max(abs(a) * xrtol, (nextafter(1.0, 2.0) - 1.0)))
@@ -128,10 +175,10 @@ proc findNonZero[T, S: SomeFloat](f: proc(x: T): S, a, barrier, xatol, xrtol: T,
 
   return x
 
-# split a < z1 < z2 < ... < zn < b into intervals (a+,z1-), (z1+, z2-), ...
-# where possible; push! onto ints
 proc makeIntervals[T, S: SomeFloat](ints: var seq[Interval[T]], f: proc(
     x: T): S, a, b: T, zs: seq[T], depth: int, xatol, xrtol: T, atol, rtol: S) =
+  ## split a < z1 < z2 < ... < zn < b into intervals (a+,z1-), (z1+, z2-), ...
+  ## where possible; push! onto ints
   var
     pts = newSeq[T](2 + zs.len)
 
@@ -153,14 +200,14 @@ proc makeIntervals[T, S: SomeFloat](ints: var seq[Interval[T]], f: proc(
 
     ints.add(Interval[T](a: ur, b: vl, depth: depth))
 
-# adjust what we mean by x1 ~ x2 for purposes of adding a new zero
 proc approxClose[T: SomeFloat](z1, z2, xatol, xrtol: T): bool =
+  ## adjust what we mean by x1 ~ x2 for purposes of adding a new zero
   let
     tol = max(sqrt(xatol), max(abs(z1), abs(z2)) * sqrt(xrtol))
   return abs(z1 - z2) < tol
 
-# is proposed not near xs? (false and we add proposed)
 proc notNear[T: SomeFloat](proposed: T, xs: seq[T], xatol, xrtol: T): bool =
+  ## is proposed not near xs? (false and we add proposed)
   let
     n = xs.len
   if n <= 1:
@@ -192,6 +239,13 @@ proc findZeros*[T, S: SomeFloat](f: proc(x: T): S, a, b: T, noPts: int = 12,
     k: int = 8, naive: bool = false, xatol: T = pow(eps(a), 0.8),
         xrtol: T = eps(a), atol: S = eps(float(1.0)), rtol: S = eps(float(
             1.0))): seq[T] =
+  ## Search for zeros of `f` in the interval `[a,b]`.
+  ##
+  ## Arguments:
+  ##
+  ## * noPts: creates noPts - 1 subintervals in the interval `[a,b]`
+  ## * k: number of pairs of intermediate points checked for a bracket
+  ## * naive: naive or an adaptive search
   var
     (a0, b0) = (float(a), float(b))
   if classify(a0) == fcNegInf:
